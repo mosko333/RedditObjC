45028224d6bf19b067ce47a231f024e8d0977053
AdamAM <adamam@gmail.com>
2018-06-08 09:12:12 -0600

    Initial Commit

---
+++ RedditObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedditObjC
+//
+//  Created by Adam on 08/06/2018.
+//  Copyright © 2018 Adam Moskovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RedditObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RedditObjC
+//
+//  Created by Adam on 08/06/2018.
+//  Copyright © 2018 Adam Moskovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
